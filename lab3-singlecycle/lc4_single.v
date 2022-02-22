bc1b98a04f4f0da5a8eaca611d5741e8ebf5e7a8
Allison Hosler <ashosler@big16.seas.upenn.edu>
2022-02-21 20:25:22 -0500

    finished implementing single.v, did not attempt to resolve bugs

--- lab3-singlecycle/lc4_single.v
+++ lab3-singlecycle/lc4_single.v
@@ -1,4 +1,5 @@
-/* TODO: name and PennKeys of all group members here
+/* Names: Allison Hosler and Antaures Jackson
+ * Pennkeys: ashosler and ajj4311
  *
  * lc4_single.v
  * Implements a single-cycle data path
@@ -65,9 +66,94 @@ module lc4_processor
 
 
    /*******************************
-    * TODO: INSERT YOUR CODE HERE *
+    * TODO: START YOUR CODE HERE *
     *******************************/
 
+   // Instantiate Decoder
+   wire [2:0] 	 r1sel, r2sel, wsel;
+   wire 	 r1re, r2re, regfile_we, nzp_we, select_pc_plus_one, is_load, is_store, is_branch, is_control_insn;
+   lc4_decoder d0 (.insn(i_cur_insn),
+		   .r1sel(r1sel),
+		   .r1re(r1re),
+		   .r2sel(r2sel),
+		   .r2re(r2re),
+		   .wsel(wsel),
+		   .regfile_we(regfile_we),
+		   .nzp_we(nzp_we),
+		   .select_pc_plus_one(select_pc_plus_one),
+		   .is_load(is_load),
+		   .is_store(is_store),
+		   .is_branch(is_branch),
+		   .is_control_insn(is_control_insn));
+
+   // Register Data Wires
+   wire [15:0] 	 rs_data, rt_data, rd_data, alu_result;
+      
+   // Instantiate Register File
+   lc4_regfile f0 (.clk(clk),
+		   .gwe(gwe),
+		   .rst(rst),
+		   .i_rs(r1sel),
+		   .o_rs_data(rs_data),
+		   .i_rt(r2sel),
+		   .o_rt_data(rt_data),
+		   .i_rd(wsel),
+		   .i_wdata(rd_data),
+		   .i_rd_we(regfile_we));
+   
+   // Instantiate ALU                                            
+   lc4_alu a0 (.i_insn(i_cur_insn),
+               .i_pc(pc),
+               .i_r1data(rs_data),
+               .i_r2data(rt_data),
+               .o_result(alu_result));
+   
+   // Data Memory
+   assign o_dmem_addr = alu_result;
+   assign o_dmem_towrite = rt_data;
+   assign o_dmem_we = is_store;
+
+   // regInputMux 
+   wire 	 [15:0] pc_inc;
+   cla16 c0(.a(pc),
+	    .b(16'b0),
+	    .cin(1'b1),
+	    .sum(pc_inc));
+   assign rd_data = is_load == 1'b1 ? i_cur_dmem_data :
+		    select_pc_plus_one == 1'b1 ? pc_inc :
+		    alu_result; 
+
+   // NZP Register (CMPs nzp_we)
+   wire [15:0] 		nzp;
+   Nbit_reg #(16) nzp_reg (.in(alu_result), .out(nzp), .clk(clk), .we(nzp_we), .gwe(gwe), .rst(rst));
+   
+   // Branch Unit
+   wire	is_true_branch;
+   wire [2:0] branch_type = i_cur_insn[11:9];
+   assign is_true_branch = (nzp == 16'b0) & (branch_type[1] == 1'b1) ? 1'b1: //z
+			   (nzp == 16'b1) & (branch_type[0] == 1'b1) ? 1'b1: //p
+			   (nzp == 16'hffff) & (branch_type[2] == 1'b1) ? 1'b1: //n
+			   1'b0;
+   assign o_cur_pc = (is_branch & is_true_branch) | is_control_insn ? alu_result : pc_inc;
+   assign next_pc = o_cur_pc;
+		   
+   // Assign test wires
+   assign test_cur_pc = pc;
+   assign test_cur_insn = i_cur_insn;
+   assign test_regfile_we = regfile_we;
+   assign test_regfile_wsel = wsel;
+   assign test_regfile_data = rd_data;
+   assign test_nzp_we = nzp_we;
+   assign test_nzp_new_bits = alu_result;
+   assign test_dmem_we = o_dmem_we;
+   assign test_dmem_addr = o_dmem_addr;
+   assign test_dmem_data = rd_data;
+   
+   
+   
+   /*******************************   
+    * TODO: END YOUR CODE HERE *
+    *******************************/
 
 
    /* Add $display(...) calls in the always block below to
