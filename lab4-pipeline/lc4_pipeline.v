a28fa59f32ab48363a49a261a36648baa133e33f
Antaures Jackson <ajj4311@wharton.upenn.edu>
2022-03-31 21:02:46 -0400

    initial decode stage finished

--- lab4-pipeline/lc4_pipeline.v
+++ lab4-pipeline/lc4_pipeline.v
@@ -50,13 +50,14 @@ module lc4_processor
     // Program counter register should start at 8200 at bootup
     Nbit_reg #(16, 16'h8200) pc_reg (.in(next_pc), .out(pc), .clk(clk), .we(pc_we), .gwe(gwe), .rst(rst));
 
-    // Stall wires
+    // Stall wires and flush wires
     wire [1:0] fd_stall, de_stall, em_stall, mw_stall;
+    wire fet_flush, fd_flush; // Not sure if we need more
 
 
    // ======================== DECODE Stage ==============================
    // Wires for fetch to decode register
-    wire [15:0] DEC_pc_inc, pc_inc;
+    wire [15:0] DEC_pc_inc, DEC_insn, pc_inc;
     wire fd_we;
 
     // Increment pc to pass through registers
@@ -65,11 +66,49 @@ module lc4_processor
 	    .cin(1'b1),
 	    .sum(pc_inc));
 
-    // Register for fetch to decode
+    // Register(s) for fetch to decode
     Nbit_reg #(16) FD_PCINC(.out(DEC_pc_inc), .in(pc_inc), .clk(clk), .we(fd_we), .gwe(gwe), .rst(rst));
     Nbit_reg #(16) FD_INSN(.out(DEC_insn), .in(i_cur_insn), .clk(clk), .we(fd_we), .gwe(gwe), .rst(rst));
     Nbit_reg #(2, 2'b10) (.out(fd_stall), .in(mw_stall), .clk(clk), .we(fd_we), .gwe(gwe), .rst(rst)); // NOT SURE if mw_stall should be input here
-    Nbit_reg #(1) (.out(fd_flush))
+    Nbit_reg #(1) (.out(fd_flush), .in(fet_flush), .clk(clk), .we(fd_we), .gwe(gwe), .rst(rst));
+
+    // Wires for decoder
+    wire [2:0] r1sel, r2sel, wsel; // 3 3-bit select wires
+    wire  r1re, r2re, regfile_we, nzp_we, select_pc_plus_one, is_load, is_store, is_branch, is_control_insn;  // 9 single bit control wires
+
+    // Instantiate decoder
+    lc4_decoder Decoder (.insn(DEC_insn),
+		   .r1sel(r1sel),
+		   .r1re(r1re),
+		   .r2sel(r2sel),
+		   .r2re(r2re),
+		   .wsel(wsel),
+		   .regfile_we(regfile_we),
+		   .nzp_we(nzp_we),
+		   .select_pc_plus_one(select_pc_plus_one),
+		   .is_load(is_load),
+		   .is_store(is_store),
+		   .is_branch(is_branch),
+		   .is_control_insn(is_control_insn));
+
+    // Register data wires
+    wire [15:0] rs_data, rt_data, rd_data, alu_result;
+
+    // Instantiate register file
+    lc4_regfile RegisterFile (.clk(clk),
+		   .gwe(gwe),
+		   .rst(rst),
+		   .i_rs(r1sel),
+		   .o_rs_data(rs_data),
+		   .i_rt(r2sel),
+		   .o_rt_data(rt_data),
+		   .i_rd(wsel),
+		   .i_wdata(rd_data),
+		   .i_rd_we(regfile_we));
+
+    // ========================= EXECUTE Stage ===========================
+    // Register(s) for decode to execute
+    Nbit_reg #(16) 
  
     /* You may also use if statements inside the always block
     * to conditionally print out information.
