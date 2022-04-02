a6dbdcdce6244b9994d8e765a13c0abc45643648
Antaures Jackson <ajj4311@wharton.upenn.edu>
2022-04-01 23:27:22 -0400

    testing

--- lab4-pipeline/testbench_lc4_processor.v
+++ lab4-pipeline/testbench_lc4_processor.v
@@ -2,13 +2,13 @@
 `default_nettype none
 
 // set this to 1 to exit after the first failure
-`define EXIT_AFTER_FIRST_ERROR 0
+`define EXIT_AFTER_FIRST_ERROR 1
 
 // change this to adjust how many errors are printed out
 `define MAX_ERRORS_TO_DISPLAY 15
 
 // set this to 1 to create a waveform file for easier debugging
-`define GENERATE_VCD 0
+`define GENERATE_VCD 1
 
 
 `define EOF 32'hFFFF_FFFF
